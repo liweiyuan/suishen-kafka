5935fb6c37f1b99af1cbc036bf541e39ea37c6c1
liweiyuan <liweiyuan@etouch.cn>
2019-08-22 11:22:51 +0800

    enhancement:添加手动确认消息消费

--- suishen-kafka-producer/src/main/java/suishen/kafka/producer/config/KafkaConfig.java
+++ suishen-kafka-producer/src/main/java/suishen/kafka/producer/config/KafkaConfig.java
@@ -26,6 +26,7 @@ public class KafkaConfig {
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        //config.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG)
 
         return new DefaultKafkaProducerFactory<>(config);
     }
